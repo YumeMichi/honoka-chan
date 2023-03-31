07e1bc7314011ead92dc3cd9400d2d9049180bd9
Yuan Si <do4suki@gmail.com>
2023-04-01 04:15:09 +0800

    Use router groups
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- main.go
+++ main.go
@@ -20,55 +20,65 @@ func main() {
 	if config.Conf.SifCap.Enabled {
 		sifcap.Start()
 	} else {
-		// router
+		// Router
 		r := gin.Default()
-		r.Use(middleware.KlabHeader)
 
-		// Private APIs
-		r.GET("/v1/basic/getcode", handler.GetCodeHandler)
-		r.POST("/v1/account/active", handler.ActiveHandler)
-		r.POST("/v1/basic/publickey", handler.PublicKeyHandler)
-		r.POST("/v1/basic/handshake", handler.HandshakeHandler)
-		r.POST("/v1/account/initialize", handler.InitializeHandler)
-		r.POST("/v1/account/login", handler.AccountLoginHandler)
-		r.POST("/v1/account/loginauto", handler.LoginAutoHandler)
-		r.POST("/v1/basic/loginarea", handler.LoginAreaHandler)
-		r.POST("/v1/account/reportRole", handler.ReportRoleHandler)
-		r.POST("/v1/basic/getProductList", handler.GetProductListHandler)
-		r.POST("/report/ge/app", handler.ReportLog)
-		r.GET("/agreement/all", handler.AgreementHandler)
-		r.GET("/integration/appReport/initialize", handler.ReportApp)
-		// Private APIs
-
+		// /
 		r.Any("/", func(ctx *gin.Context) {
 			ctx.String(http.StatusOK, "Hello, world!")
 		})
-		r.GET("/webview.php/announce/index", handler.AnnounceIndexHandler)
-		r.POST("/main.php/login/authkey", handler.AuthKeyHandler)
-		r.POST("/main.php/login/login", handler.LoginHandler)
-		r.POST("/main.php/user/userInfo", handler.UserInfoHandler)
-		r.POST("/main.php/gdpr/get", handler.GdprHandler)
-		r.POST("/main.php/personalnotice/get", handler.PersonalNoticeHandler)
-		r.POST("/main.php/tos/tosCheck", handler.TosCheckHandler)
-		r.POST("/main.php/download/batch", handler.DownloadBatchHandler)
-		r.POST("/main.php/download/event", handler.DownloadEventHandler)
-		r.POST("/main.php/lbonus/execute", handler.LBonusExecuteHandler)
-		r.POST("/main.php/api", handler.ApiHandler)
-		r.POST("/main.php/announce/checkState", handler.AnnounceCheckStateHandler)
-		r.POST("/main.php/scenario/startup", handler.ScenarioStartupHandler)
-		r.POST("/main.php/scenario/reward", handler.ScenarioRewardHandler)
-		r.POST("/main.php/user/setNotificationToken", handler.SetNotificationTokenHandler)
-		r.POST("/main.php/user/changeNavi", handler.SetNotificationTokenHandler)
-		r.POST("/main.php/event/eventList", handler.EventListHandler)
-		r.POST("/main.php/payment/productList", handler.ProductListHandler)
-		r.POST("/main.php/live/partyList", handler.PartyListHandler)
-		r.POST("/main.php/live/play", handler.PlayLiveHandler)
-		r.POST("/main.php/live/gameover", handler.GameOverHandler)
-		r.POST("/main.php/unit/setDisplayRank", handler.SetDisplayRankHandler)
-		r.POST("/main.php/unit/favorite", handler.SetDisplayRankHandler)
-		r.POST("/main.php/subscenario/startup", handler.SubScenarioStartupHandler)
-		r.POST("/main.php/subscenario/reward", handler.SubScenarioStartupHandler)
-		r.POST("/main.php/album/seriesAll", handler.AlbumSeriesAllHandler)
+
+		// Private APIs
+		v1 := r.Group("v1")
+		{
+			v1.GET("/basic/getcode", handler.GetCodeHandler)
+			v1.POST("/account/active", handler.ActiveHandler)
+			v1.POST("/basic/publickey", handler.PublicKeyHandler)
+			v1.POST("/basic/handshake", handler.HandshakeHandler)
+			v1.POST("/account/initialize", handler.InitializeHandler)
+			v1.POST("/account/login", handler.AccountLoginHandler)
+			v1.POST("/account/loginauto", handler.LoginAutoHandler)
+			v1.POST("/basic/loginarea", handler.LoginAreaHandler)
+			v1.POST("/account/reportRole", handler.ReportRoleHandler)
+			v1.POST("/basic/getProductList", handler.GetProductListHandler)
+		}
+		r.GET("/agreement/all", handler.AgreementHandler)
+		r.GET("/integration/appReport/initialize", handler.ReportApp)
+		r.POST("/report/ge/app", handler.ReportLog)
+		// Private APIs
+
+		// Server APIs
+		m := r.Group("main.php").Use(middleware.KlabHeader)
+		{
+			m.POST("/login/authkey", handler.AuthKeyHandler)
+			m.POST("/login/login", handler.LoginHandler)
+			m.POST("/user/userInfo", handler.UserInfoHandler)
+			m.POST("/gdpr/get", handler.GdprHandler)
+			m.POST("/personalnotice/get", handler.PersonalNoticeHandler)
+			m.POST("/tos/tosCheck", handler.TosCheckHandler)
+			m.POST("/download/batch", handler.DownloadBatchHandler)
+			m.POST("/download/event", handler.DownloadEventHandler)
+			m.POST("/lbonus/execute", handler.LBonusExecuteHandler)
+			m.POST("/api", handler.ApiHandler)
+			m.POST("/announce/checkState", handler.AnnounceCheckStateHandler)
+			m.POST("/scenario/startup", handler.ScenarioStartupHandler)
+			m.POST("/scenario/reward", handler.ScenarioRewardHandler)
+			m.POST("/user/setNotificationToken", handler.SetNotificationTokenHandler)
+			m.POST("/user/changeNavi", handler.SetNotificationTokenHandler)
+			m.POST("/event/eventList", handler.EventListHandler)
+			m.POST("/payment/productList", handler.ProductListHandler)
+			m.POST("/live/partyList", handler.PartyListHandler)
+			m.POST("/live/play", handler.PlayLiveHandler)
+			m.POST("/live/gameover", handler.GameOverHandler)
+			m.POST("/unit/setDisplayRank", handler.SetDisplayRankHandler)
+			m.POST("/unit/favorite", handler.SetDisplayRankHandler)
+			m.POST("/subscenario/startup", handler.SubScenarioStartupHandler)
+			m.POST("/subscenario/reward", handler.SubScenarioStartupHandler)
+			m.POST("/album/seriesAll", handler.AlbumSeriesAllHandler)
+
+		}
+		r.GET("/webview.php/announce/index", handler.AnnounceIndexHandler, middleware.KlabHeader)
+		// Server APIs
 
 		r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	}
